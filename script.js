80824a809d6c38e4919693cb717f277a258a070b
algpy99 <96131606+algpy99@users.noreply.github.com>
2024-12-30 17:34:10 +0100

    basic landing page

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.right-box .box').forEach(box => {
-    box.addEventListener('mouseenter', function() {
-      const imageSrc = this.getAttribute('data-image');
-      document.getElementById('left-box-img').src = imageSrc;
-    });
-  });
-  
